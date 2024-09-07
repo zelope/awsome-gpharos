dae7e62d120b99124c8c6c78bcb2ad485fc7a94b
zelope <ljg980214@gmail.com>
2024-09-08 01:52:22 +0900

    test

--- trackboard/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrackboardConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "trackboard"
--- trackboard/views.py
+++
@@ -1,6 +0,0 @@
-# dashboard/views.py
-
-from django.shortcuts import render
-
-def dashboard(request):
-    return render(request, 'trackboard/dashboard.html')
